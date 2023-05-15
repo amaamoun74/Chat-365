37cdd6d137826fc8e439ab5835975240725096c4
ahmed maamoun <amaamoun74@gmail.com>
2023-05-15 22:33:23 +0300

    ui

--- Chat-365/View/ViewController/AuthenticationViewController.swift
+++ Chat-365/View/ViewController/AuthenticationViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class AuthenticationViewController: UIViewController {
 
+    var isLogin: Bool = false
     
     @IBOutlet weak var viewTitle: UILabel!
     
@@ -21,14 +22,17 @@ class AuthenticationViewController: UIViewController {
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var confirmPassword: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var questionLable: UILabel!
     
-    
+   
     @IBOutlet weak var questionBtn: UIButton!
+    @IBOutlet weak var forgetPasswordBtn: UIButton!
+    @IBOutlet weak var resendEMailBtn: UIButton!
     @IBOutlet weak var authenticationBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        emailLabel.text = ""
+        emailLabel.text = " "
         passwordLabel.text = ""
         confirmPasswordLabel.text = ""
         
@@ -39,21 +43,53 @@ class AuthenticationViewController: UIViewController {
 
     @IBAction func resendEmail(_ sender: Any) {
     }
-    
-    @IBAction func authenticate(_ sender: Any) {
+    @IBAction func forgetPassword(_ sender: Any) {
+    }
+   
+    @IBAction func resendEmailAction(_ sender: Any) {
     }
     
+    @IBAction func authenticate(_ sender: Any) {
+        
+    }
     
-    @IBAction func setQuestionAction(_ sender: Any) {
+    @IBAction func questionAction(_ sender: Any) {
     }
 }
 
 // MARK: - text Field Delegate
-
 extension AuthenticationViewController : UITextFieldDelegate {
     func textFieldDidChangeSelection(_ textField: UITextField) {
         emailLabel.text = email.hasText ? "Email" : ""
         passwordLabel.text = password.hasText ? "Password" : ""
         confirmPasswordLabel.text = confirmPassword.hasText ? "Confirm Password" : ""
+    }
+}
+
+// MARK: - functions
+extension AuthenticationViewController {
+    private func updateUIMode(mode: Bool){
+        if !mode {
+            viewTitle.text = "Login"
+            confirmPassword.isHidden = true
+            confirmPasswordLabel.isHidden = true
+            resendEMailBtn.isHidden = true
+            forgetPasswordBtn.isHidden = false
+            authenticationBtn.setTitle("Login", for: .normal)
+            questionBtn.setTitle("Register", for: .normal)
+        questionLable.text = "Do you have an account?"
+        }
+        else{
+                viewTitle.text = "Register"
+                confirmPassword.isHidden = false
+            confirmPasswordLabel.isHidden = false
+                resendEMailBtn.isHidden = false
+                forgetPasswordBtn.isHidden = true
+                authenticationBtn.setTitle("Register", for: .normal)
+                questionBtn.setTitle("Login", for: .normal)
+            questionLable.text = "Create Account?"
+        }
+        
+        isLogin.toggle()
     }
 }
