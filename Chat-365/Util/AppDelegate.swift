fbc7d9703e4bfc952f237b64755a36bd9bf5cdd8
ahmed maamoun <amaamoun74@gmail.com>
2023-05-15 19:37:25 +0300

    configuration

--- Chat-365/Util/AppDelegate.swift
+++ Chat-365/Util/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-//import FirebaseCore
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //FirebaseApp.configure()
+        FirebaseApp.configure()
         return true
     }
 
@@ -33,6 +33,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    // MARK: - prevent rotation
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask{
+            return .portrait
+    }
 
 }
 
